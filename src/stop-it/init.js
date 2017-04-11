bd5e6509192885c0e409a5e2e5cd6b1b7e50f4ac
Hayden Andreyka <haydenandreyka@gmail.com>
2017-04-11 15:03:30 -0700

    refactor init script

--- src/stop-it/init.js
+++ src/stop-it/init.js
@@ -1,47 +1,39 @@
 // inits things like config, modules, and other stuff
 
-var tasks = [
-	// config loading
-	new Promise((resolve, reject) => {
-		var result;
-		try {
-			result = fs.readFileSync(__dirname + '/../cfg/config.json', 'utf8');
-		} catch(er) {
-			return reject('Failed to access config file. Please ensure config.json exists and is accessible you fucking idiot.');
-		}
-			
-		global.Config = JSON.parse(result);
-		return resolve();
-	}),
+// config loading
+new Promise((resolve, reject) => {
+	var result;
+	try {
+		result = fs.readFileSync(__dirname + '/../cfg/config.json', 'utf8');
+	} catch(er) {
+		return reject('Failed to access config file. Please ensure config.json exists and is accessible you fucking idiot.');
+	}
+		
+	global.Config = JSON.parse(result);
+	console.log(`Loaded config...`);
+	return resolve();
+}).then(
 
-	// winston init
-	new Promise((resolve, reject) => {
-		var logdir = __dirname + '/../' + Config.Logging.Container;
-		
-		if (!fs.existsSync(logdir))
-			fs.mkdirSync(logdir);
-		
-		global.logger = new winston.Logger({
-			level: Config.Logging.Level,
-			transports: [
-				new (winston.transports.Console)(),
-				new wdrf({
-					filename: path.join(Config.Logging.Container, Config.Logging.Prefix),
-					datePattern: Config.Logging.DatePattern + '.log',
-					json: Config.Logging.JSON
-				})
-			]
-		});
-	})
-];
+// winston init
+new Promise((resolve, reject) => {
+	var logdir = __dirname + '/../' + Config.Logging.Container;
 	
-// run all tasks sequentially
-Promise.all(tasks).then(doneInit).catch((err) => {
-	// the fucky logic in this next line is equivalent to (err.stack ? err.stack : err)
+	if (!fs.existsSync(logdir))
+		fs.mkdirSync(logdir);
+	
+	global.logger = new winston.Logger({
+		level: Config.Logging.Level,
+		transports: [
+			new (winston.transports.Console)(),
+			new wdrf({
+				filename: path.join(Config.Logging.Container, Config.Logging.Prefix),
+				datePattern: Config.Logging.DatePattern + '.log',
+				json: Config.Logging.JSON
+			})
+		]
+	});
+	logger.debug('Winston initialized!');
+})).catch(err => {	// error doing something
 	console.error(`Init error: ${(err.stack||err)}`);
 	process.exit(1);
 });
-
-function doneInit() {
-	logger.error('Done initializing program!');
-}
