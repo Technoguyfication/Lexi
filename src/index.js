469415fdebe517165a4cf94f6849c9d963df9695
Hayden Andreyka <haydenandreyka@gmail.com>
2017-04-10 17:03:47 -0700

    some stuff

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// stop-it bot for discord
+// copyright 2017 hayden andreyka - technoguyfication(.com) <haydenandreyka@gmail.com>
+// all rights are implied under the absence of a formal open source software license
+
+try {
+	global.fs = require('fs');
+	global.util = require('util');
+} catch(er) {
+	console.error(`Fatal error loading dependencies:\n\n${er.stack}`);
+	process.exit(1);
+}
+require('./stop-it/stop-it.js');
