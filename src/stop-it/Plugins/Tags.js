cc61b66199b4a77869c8a92936e9fce84c5f28aa
Hayden Andreyka <haydenandreyka@gmail.com>
2017-04-12 18:32:35 -0700

    give this some more structure

--- src/stop-it/Plugins/Tags.js
+++ src/stop-it/Plugins/Tags.js
@@ -1,20 +1,29 @@
 /*
 	Tags
-	
-	Author: technoguyfication <haydenandreyka@gmail.com>
 */
 
 const pluginInfo = {
 	name: 'Tags',
-	author: 'Technoguyfication',
+	author: 'Hayden Andreyka <haydenandreyka@gmail.com>',
 	version: '1.0.0',
 	commands: ['tag', 'tags', 'createtag', 'addtag', 'deletetag', 'deltag', 'removetag']
 };
 
+// formal entry for plugin
 class Tags extends Plugin {
 	constructor() {
 		super(pluginInfo);
 	}
+	
+	// override
+	onEnable() {
+		message('Enabling Tags...');
+		// perform db checks, etc. here
+		message('Tags enabled!');
+	}
 }
 module.exports = Tags;
 
+function message(msg) {
+	logger.info(`[Tags] ${msg}`);
+}
