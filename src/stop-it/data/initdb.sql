22820292c25bd5b11c7660ae1ad4e7298198efa1
Hayden Andreyka <haydenandreyka@gmail.com>
2017-04-12 17:01:53 -0700

    move tag db stuff lol

--- src/stop-it/data/initdb.sql
+++ src/stop-it/data/initdb.sql
@@ -38,14 +38,3 @@ CREATE TABLE IF NOT EXISTS `stats` (
 	`errors` INT,	-- number of exceptions generated
 	PRIMARY KEY (`id`)
 );
-
--- tags
-CREATE TABLE IF NOT EXISTS `tags` (
-	`id` INT NOT NULL AUTO_INCREMENT,	-- unique id for each tag
-	`server` VARCHAR(25) NOT NULL, -- server snowflake that created the tag
-	`channel` VARCHAR(25) NOT NULL,	-- same as above; both used to delete the tag if it's orphaned
-	`title` VARCHAR(50),	-- title of tag
-	`text` TEXT,	-- tag content
-	PRIMARY KEY (`id`),
-	INDEX (`title`)
-);
