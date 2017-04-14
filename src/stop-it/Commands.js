8a14eb99928522d80866186752d629e946c5ca58
Hayden Andreyka <haydenandreyka@gmail.com>
2017-04-14 13:55:58 -0700

    start work on command manager

---
+++ src/stop-it/Commands.js
@@ -0,0 +1,11 @@
+// command manager that has everything to do with commands
+
+const builtinCommands = [
+	'help',
+	'plugins', 'plugin', 'pl',
+	'database', 'db',
+	'stats', 'stat',
+	'eval', 'exec', 'update'
+];
+
+
