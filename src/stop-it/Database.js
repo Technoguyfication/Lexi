b1a0e85d09864e162ad3cde291b0ab0a6f44d532
Hayden Andreyka <haydenandreyka@gmail.com>
2017-04-14 11:02:37 -0700

    database is complete

---
+++ src/stop-it/Database.js
@@ -0,0 +1,68 @@
+/*
+	handles all database operations
+*/
+
+const mysqlConfig = {
+	host: Config.MySQL.Hostname,
+	user: Config.MySQL.Authentication.User,
+	password: Config.MySQL.Authentication.Password,
+	database: Config.MySQL.Database,
+	multipleStatements: true
+};
+
+var Connections;
+
+const initScript = __dirname + '/./Data/initdb.sql';
+
+function Init() {
+	return new Promise((resolve, reject) => {
+		Connections = mysql.createPool(mysqlConfig);
+		logger.silly('pool created');
+		initDb().then(resolve, reject);
+	});
+}
+module.exports.Init = Init;
+
+function Query(sql, values) {
+	return new Promise((resolve, reject) => {
+		logger.silly(`Running database query: ${sql} with values ${values}`);
+
+		Connections.query(sql, values, processOutput);
+
+		function processOutput(err, results, fields) {
+			if (err)
+				return reject(err);
+
+			return resolve(results, fields);
+		}
+	});
+}
+module.exports.Query = Query;
+
+function Terminate() {
+	return new Promise((resolve, reject) => {
+		if (!Connections)
+			return resolve();
+
+		Connections.end(err => {
+			if (err)
+				return reject(err);
+			else return resolve();
+		});
+	});
+}
+module.exports.Terminate = Terminate();
+
+function initDb() {
+	return new Promise((resolve, reject) => {
+		fs.readFile(initScript, 'utf8', (err, data) => {
+			if (err)
+				return reject(err);
+
+			Query(data, null).then(() => {
+				logger.debug('Database structure initialized.');
+				return resolve();
+			}, reject);
+		});
+	});
+}
